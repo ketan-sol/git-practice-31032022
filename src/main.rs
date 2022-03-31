a721a5e3c71d694493bebbc2aac9b68eebfd8f9c
ketan-sol <ketam@solulab.com>
2022-03-31 20:23:17 +0530

    added hello world

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,3 @@
-use std::io;
 fn main() {
-    println!("Enter weight (kg): ");
-    let mut input = String::new();
-    // some_fn(&mut input);
-    io::stdin().read_line(&mut input).unwrap();
-
-    let weight: f32 = input.trim().parse().unwrap();
-    // dbg!(weight);
-
-    // println!("Input: {}", input);
-    let m_weight = weight_on_mars(weight);
-    println!("Weight on Mars: {}kg ", m_weight);
+    println!("Hello world")
 }
-
-fn weight_on_mars(weight: f32) -> f32 {
-    (weight / 9.81) * 3.711
-}
-
-//fn some_fn(s: &mut String) {}
