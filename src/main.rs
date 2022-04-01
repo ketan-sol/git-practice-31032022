642e7561f892bece068cd1007a39a5fe076b058b
ketan-sol <ketam@solulab.com>
2022-04-01 18:06:23 +0530

    enums

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,8 @@
 fn main() {
+    // let get = Method::GET;
+    // let delete = Method::DELETE;
+    // let post = Method::POST;
+
     let server = Server::new("127.0.0.1:8080".to_string());
     server.run();
 }
@@ -15,4 +19,22 @@ impl Server {
     fn run(self) {
         println!("Listening on {}", self.addr);
     }
+}
+
+struct Request {
+    path: String,
+    query_string: Option<String>,
+    method: Method,
+}
+
+enum Method {
+    GET,
+    DELETE,
+    POST,
+    PUT,
+    HEAD,
+    CONNECT,
+    OPTIONS,
+    TRACE,
+    PATCH,
 }
