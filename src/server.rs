f41b9efbb38939ee4c844072aff796734f740094
ketan-sol <ketam@solulab.com>
2022-04-04 17:52:59 +0530

    suceesfully sent http request from browser

--- src/server.rs
+++ src/server.rs
@@ -1,8 +1,8 @@
+use std::io::Read;
 use std::net::TcpListener;
 pub struct Server {
     addr: String,
 }
-
 impl Server {
     pub fn new(addr: String) -> Self {
         Self { addr }
@@ -12,5 +12,27 @@ impl Server {
         println!("Listening on {}", self.addr);
 
         let listener = TcpListener::bind(&self.addr).unwrap();
+
+        loop {
+            match listener.accept() {
+                Ok((mut stream, _)) => {
+                    let mut buffer = [0; 1024];
+                    match stream.read(&mut buffer) {
+                        Ok(_) => {
+                            println!("Received a request: {}", String::from_utf8_lossy(&buffer));
+                        }
+                        Err(e) => println!("error: {}", e),
+                    }
+                }
+                Err(e) => println!("Error: {}", e),
+            }
+            // let res = listener.accept();
+
+            // if res.is_err() {
+            //     continue;
+            // }
+
+            // let (stream, addr) = res.unwrap();
+        }
     }
 }
