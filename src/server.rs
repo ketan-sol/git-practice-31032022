8aa049bf262ab3d5b9ab79e6475732ee428c580b
ketan-sol <ketam@solulab.com>
2022-04-04 13:09:40 +0530

    added tcpListener to server

--- src/server.rs
+++ src/server.rs
@@ -1,3 +1,4 @@
+use std::net::TcpListener;
 pub struct Server {
     addr: String,
 }
@@ -9,5 +10,7 @@ impl Server {
 
     pub fn run(self) {
         println!("Listening on {}", self.addr);
+
+        let listener = TcpListener::bind(&self.addr).unwrap();
     }
 }
